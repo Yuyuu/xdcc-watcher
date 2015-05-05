33fafafe71029a6ca6ca83ca190d86e3e46f0ef9
Vincent Tertre <vincent.tertre@gmail.com>
2015-05-05 20:32:13 +0200

    Configure Logback

--- src/main/java/fr/vter/xdcc/schedule/quartz/BaseSchedule.java
+++ src/main/java/fr/vter/xdcc/schedule/quartz/BaseSchedule.java
@@ -3,8 +3,22 @@ package fr.vter.xdcc.schedule.quartz;
 import org.quartz.Scheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+
+import java.util.logging.Handler;
+import java.util.logging.LogManager;
 
 public abstract class BaseSchedule {
+
+  public BaseSchedule() {
+    // Deactivate JUL logs
+    final java.util.logging.Logger rootLogger = LogManager.getLogManager().getLogger("");
+    final Handler[] handlers = rootLogger.getHandlers();
+    for (final Handler handler : handlers) {
+      rootLogger.removeHandler(handler);
+    }
+    SLF4JBridgeHandler.install();
+  }
 
   protected abstract void configure(Scheduler scheduler) throws Exception;
 
